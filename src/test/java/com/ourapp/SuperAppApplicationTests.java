b12a2b0fc8270db61a4e91cb7f384280eaec4fb0
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2023-02-04 16:36:31 +0200

    init

---
+++ src/test/java/com/ourapp/SuperAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
